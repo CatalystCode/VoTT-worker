7f60a319242db257d97657d8825856f00ae06698
JC Jimenez <jcjimenez@gmail.com>
2018-01-11 18:28:44 -0600

    Enhanced hello-world plugin documentation.

---
+++ plugins/hello-world/README.md
@@ -0,0 +1,3 @@
+# hello-world
+This is a placeholder plugin that can be used as an starting point or for testing the plugin invocation system (`traind.py`).
+
--- plugins/hello-world/plugin.py
+++ plugins/hello-world/plugin.py
@@ -1,4 +1,16 @@
 import threading
+import sys
+import argparse
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Hello World VoTT-train plugin.')
+    parser.add_argument('--input-annotations', help='URL to annotations csv.', default=None, type=str)
+    parser.add_argument('--output-model', help='URL to Azure Storage Container or AWS S3 bucket.', default=None, type=str)
+    parser.add_argument('--output-status', help='URL to training status callback.', default=None, type=str)
+    return parser.parse_args(args)
+
+
+args = parse_args(sys.argv[1:])
 
 hello_count = 0
 def hello():
